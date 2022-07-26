35292d339126d51c382dbd411998c831846d2199
Salim66 <salimhasanriad8@gmail.com>
2022-07-26 15:20:40 +0600

    Authenticate Middleware implement by jwt token

--- api/routes/student.js
+++ api/routes/student.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createStudent, deleteStudent, getAllStudents, getSingleStudent, updateStudent } from '../controllers/studentController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 
 // init router
@@ -7,8 +8,8 @@ const router = express.Router();
 
 
 // route
-router.route('/').get(getAllStudents).post(createStudent);
-router.route('/:id').get(getSingleStudent).put(updateStudent).patch(updateStudent).delete(deleteStudent);
+router.route('/').get(authMiddleware, getAllStudents).post(authMiddleware, createStudent);
+router.route('/:id').get(authMiddleware, getSingleStudent).put(authMiddleware, updateStudent).patch(authMiddleware, updateStudent).delete(authMiddleware, deleteStudent);
 
 
 // Export default router
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createUser, deleteUser, getAllUser, getSingleUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 
 
@@ -8,8 +9,8 @@ const router = express.Router();
 
 
 // route
-router.route('/').get(getAllUser).post(createUser);
-router.route('/:id').get(getSingleUser).put(updateUser).patch(updateUser).delete(deleteUser);
+router.route('/').get(authMiddleware, getAllUser).post(authMiddleware, createUser);
+router.route('/:id').get(authMiddleware, getSingleUser).put(authMiddleware, updateUser).patch(authMiddleware, updateUser).delete(authMiddleware, deleteUser);
 
 // Auth Route
 router.post('/login', userLogin);
