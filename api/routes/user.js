8767f71afd77aa67fd92a0295dd9d6797b365372
Salim66 <salimhasanriad8@gmail.com>
2022-07-26 15:34:24 +0600

    protect another user data access by useMiddleware

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createUser, deleteUser, getAllUser, getSingleUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { userMiddleware } from '../middleware/userMiddleware.js';
 
 
 
@@ -10,7 +11,7 @@ const router = express.Router();
 
 // route
 router.route('/').get(authMiddleware, getAllUser).post(authMiddleware, createUser);
-router.route('/:id').get(authMiddleware, getSingleUser).put(authMiddleware, updateUser).patch(authMiddleware, updateUser).delete(authMiddleware, deleteUser);
+router.route('/:id').get(userMiddleware, getSingleUser).put(userMiddleware, updateUser).patch(userMiddleware, updateUser).delete(userMiddleware, deleteUser);
 
 // Auth Route
 router.post('/login', userLogin);
