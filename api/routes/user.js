42349ddb6f98bd166bd63a76162f5e5779871f2c
Salim66 <salimhasanriad8@gmail.com>
2022-07-26 15:47:30 +0600

    isAdmin Middleware create

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createUser, deleteUser, getAllUser, getSingleUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
+import { adminMiddleware } from '../middleware/adminMiddleware.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { userMiddleware } from '../middleware/userMiddleware.js';
 
@@ -10,7 +11,7 @@ const router = express.Router();
 
 
 // route
-router.route('/').get(authMiddleware, getAllUser).post(authMiddleware, createUser);
+router.route('/').get(adminMiddleware, getAllUser).post(adminMiddleware, createUser);
 router.route('/:id').get(userMiddleware, getSingleUser).put(userMiddleware, updateUser).patch(userMiddleware, updateUser).delete(userMiddleware, deleteUser);
 
 // Auth Route
